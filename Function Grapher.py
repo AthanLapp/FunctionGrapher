1d511f5cc41b44beb728dfca6dd9f0dadd483948
Αθανάσιος Λάππας ( Athanasios Lappas) <Athan.Lapp@gmail.com>
2021-09-09 11:20:54 +0300

    Update Function Grapher.py

--- Function Grapher.py
+++ Function Grapher.py
@@ -6,7 +6,7 @@ description: Function Grapher with set range and discretization
 
 import matplotlib.pyplot as plt
 
-#solving the function1/2x^3+3x^2-2x+2 as an example. You can change the values disc, rnge and the function in line 22 to solve your's
+#solving the function1/2x^3+3x^2-2x+2 as an example. You can change the values disc, rnge and the function in lines 20 and 22 to solve your's
 disc=100  #precision 0.01 (disc-<discretization=1/precision),
 x=[]
 i=0
@@ -17,7 +17,7 @@ for i in range(-rnge,rnge+1):
         x.append(i+j/disc) #(with 1/disc step)
 y=[]
 for i in x:
-    y.append(1/2*i**3+3*i**2-2*i+2)
+    y.append(1/2*i**3+3*i**2-2*i+2) #Here is the function to be solved. Change it with yours
 plt.plot(x,y)
-plt.ylabel('1/2*i**3+3*i**2-2*i+2') #Here is the function to be solved. Change it with yours
+plt.ylabel('1/2*i**3+3*i**2-2*i+2') #Here is the name the function to be solved. Change it with yours
 plt.show()
